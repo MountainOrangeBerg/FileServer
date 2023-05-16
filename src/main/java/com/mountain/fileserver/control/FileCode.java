fcf753319b7e4d19e8db5c3cc5521f11f1563895
Administrator <190816935@qq.com>
2023-05-16 17:47:57 +0800

    commit

--- src/main/java/com/mountain/fileserver/control/FileCode.java
+++ src/main/java/com/mountain/fileserver/control/FileCode.java
@@ -1,12 +1,11 @@
 package com.mountain.fileserver.control;
 
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class FileCode {
@@ -17,6 +16,18 @@ public class FileCode {
         System.out.println("11111");
         HashMap hashMap = new HashMap();
         hashMap.put("detail","取件码错误，0次后将被禁止5分钟");
+        return hashMap;
+    }
+
+    @PostMapping(value = "/getFile")
+    @ResponseBody
+    public HashMap getFile(@RequestBody(required = true) JSONObject code) {//返回json 数据
+        System.out.println(code);
+        Object returnCode = code.get("code");
+        HashMap hashMap = new HashMap();
+        hashMap.put("detail","取件码错误");
+        hashMap.put("errorCode",returnCode);
+        hashMap.put("flag","error");
         return hashMap;
     }
 
